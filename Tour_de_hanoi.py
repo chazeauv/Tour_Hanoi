2738ea17938acf696421608dcfa306ddc2e5c992
Laysou05 <vincent.chazeau05@gmail.com>
2019-11-18 18:36:40 +0100

    deux nouvelles fonctions jtm

--- Tour_de_hanoi.py
+++ Tour_de_hanoi.py
@@ -1,5 +1,4 @@
 def init(n):
-	"""IL FAUT METTRE DE LA DOCUMENTATION"""
 	poteau_g = []
 	poteau_c = []
 	poteau_d = []
@@ -7,7 +6,7 @@ def init(n):
 	#dsup = [-1] #compteur disque supérieur
 
 	while n!=0:
-		poteau_g.append(n)
+		poteau_c.append(n)
 		n = n-1
 
 	#print(dsup)
@@ -19,16 +18,47 @@ def nombre_disques(n,p):
 	l = varinit[p] #Variable l qui prend l'un des poteaux en parametres
 	#print(len(l))
 
-def disque_superieur(n,p):
+def disque_superieur(n):
 
 	varinit = init(n) #variables définies dans init
-	l = varinit[p]
-	l = l[-1]
-	print(l)
+	dsup = []
+	a = varinit[0]
+	b = varinit[1]
+	c = varinit[2]
+	
+	if (a != []):
+		a = a[-1]
+		dsup.append(a)
+	else:
+		dsup.append(0)
+	if (b != []):
+		b = b[-1]
+		dsup.append(b)
+	else:
+		dsup.append(0)
+	if (c != []):
+		c = c[-1]
+		dsup.append(c)
+	else:
+		dsup.append(0)
+	print(dsup)
+def pos_disque(n,numdisque):
+
+	varinit = init(n) #variables définies dans init
+	
+	if numdisque in varinit[0]:
+		print("Le disque n°", numdisque, "est dans la tour gauche.")
+	elif numdisque in varinit[1]:
+		print("Le disque n°", numdisque, "est dans la tour centre.")
+	elif numdisque in varinit[2]:
+		print("Le disque n°", numdisque, "est dans la tour droite.")
+	else:
+		print("Le numéro de disque n'existe pas.")
+
 
 #nombre_disques(int(input("Entrez un nombre de disque : ")),(int(input("Selectionner la tour (gauche = 0, centre = 1, droite = 2): "))))
-disque_superieur(int(input("Entrez un nombre de disque : ")),(int(input("Selectionner la tour (gauche = 0, centre = 1, droite = 2): "))))
-
+disque_superieur(int(input("Entrez un nombre de disque : ")))
+#pos_disque(int(input("Entrez un nombre de disque : ")),(int(input("Entrez le numéro de disque souhaité: "))))
 
 
 
